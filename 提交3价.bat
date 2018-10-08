a7d040795a0061dec450e6f0c24c465e11004331
chaodeng <645111208@qq.com>
2018-10-08 10:13:57 +0800

    自动提交

--- 提交3价.bat
+++ 提交3价.bat
@@ -1,3 +1,2 @@
-@TortoiseProc.exe /command:update /path:"D: /kitandtool/workspace/idea-3jba/ww" /closeonend:2
-@TortoiseProc.exe /command:update /path:"D: /kitandtool/workspace/idea-3jba/nw" /closeonend:2
-
+@TortoiseProc.exe /command:commit -m "提交" /path:"D:/kitandtool/workspace/idea-3jba/nw/src/main" 
+@TortoiseProc.exe /command:commit -m "提交" /path:"D:/kitandtool/workspace/idea-3jba/ww/src/main" 
